0da1f83396a84b0a54456d0b3ed1db8c2088a11b
cchauve <cedric.chauve@sfu.ca>
2019-03-17 11:10:23 +0100

    first commit

---
+++ exp/2019-02-19__plasmid_MILP/code/plasmids_preprocessing.py
@@ -0,0 +1,208 @@
+from __future__ import division
+from gurobipy import *
+
+
+def read_file(filename):
+	string = open(filename, "r").read()
+	string_list = string.split("\n")
+	string_list = [line for line in string_list if line and line[0] != '#'] #Read line only if it is nonempty and not a comment.
+	return string_list
+
+#Storing contig details
+#-----------------------------------------------
+#Stores the id of the contig
+def get_id(line):
+	return line[1]
+#Stores the nucleotide sequence of the contig
+def get_nucleotide_seq(line):
+	#print(line[2])
+	return line[2]		
+#Computes GC ratio: counts no. of 'G'/'C' occurences in the sequence and divide by the sequence length.
+def compute_GCratio(seq):
+	GC = 0
+	ln_seq = 0
+	for nucl in seq:
+		if nucl == 'G' or nucl == 'C':
+			GC += 1
+		ln_seq += 1
+	return GC/ln_seq
+#Stores the length of the sequence
+def get_length(line):
+	return int(line[3].split(':')[2])
+#Stores the read depth of the contig
+def get_read_depth(line):
+	return float(line[4].split(':')[2])		
+
+#Takes a contig from the assembly file and initiates an entry in the contigs_dict
+#Each contig is tagged with the following attributes:
+#1. Length of the contig (int)
+#2. Overall read depth of the contig (float)
+#3. Indication if the contig is a seed (binary)
+#4. GC content of the contig (float)
+#5. Gene coverage intervals (list of pairs)
+#6. Gene coverage (float)
+def update_contigs_dict(contigs_dict, line):
+	c = get_id(line)
+	seq = get_nucleotide_seq(line) 
+	GC_cont = compute_GCratio(seq)
+	ln = get_length(line)
+	rd = get_read_depth(line)
+
+	contigs_dict[c] = {}
+	contigs_dict[c]['Sequence'] = seq
+	contigs_dict[c]['Length'] = ln
+	contigs_dict[c]['Read_depth'] = rd
+	contigs_dict[c]['Seed'] = 0							#Default
+	contigs_dict[c]['GC_cont'] = GC_cont
+	contigs_dict[c]['Gene_coverage_intervals'] = []		#Default
+	contigs_dict[c]['Gene_coverage'] = 0				#Default
+	return contigs_dict
+
+#A link is of the type: ((l1, e1),(l2, e2)) 
+#where l1, l2 are adjacent links and e1, e2 are the connected link extremities
+def get_link(line):
+	c1, o1, c2, o2 = line[1], line[2], line[3], line[4]
+	if o1 == '+':
+		ext1 = 'h'
+	else:
+		ext1 = 't'
+	if o2 == '+':
+		ext2 = 't'
+	else:
+		ext2 = 'h'
+	e = ((c1, ext1),(c2, ext2))	
+	return e 
+
+#Reads the assembly file line by line and forwards a line 
+#to update_contigs_dict or get_link depending on the entry
+def get_data(assembly_file, contigs_dict, links_list):
+	string_list = read_file(assembly_file)
+	count_s = 0
+	count_l = 0
+	for line in string_list:
+		line = line.split("\t")
+		if line[0] == 'S':
+			contigs_dict = update_contigs_dict(contigs_dict, line)
+		elif line[0] == 'L':
+			e = get_link(line)
+			links_list.append(e)
+	return contigs_dict, links_list
+
+#Reads the seed file and makes a list of seeds
+def get_seeds(seeds_file, seeds_set):
+	string_list = read_file(seeds_file)
+	for line in string_list:
+		line = line.split("\t")
+		seeds_set.add(line[0])
+	return seeds_set
+
+#Takes the gene covering intervals for a contig and finds their union
+#The length of the union is used to compute gene coverage
+def get_union(intervals):
+	union = []
+	for begin, end in sorted(intervals):
+		if union and union[-1][1] >= begin - 1:
+			union[-1][1] = max(union[-1][1], end)
+		else:
+			union.append([begin, end])
+	return union		
+
+#Computes the gene coverage for each contig
+def get_gene_coverage(mapping_file, contigs_dict):
+	string_list = read_file(mapping_file)
+	possible_seeds = []
+	for line in string_list:
+		line = line.split("\t")	
+		qseqid, sseqid = line[0], line[1]
+		sstart, send = line[8], line[9]
+		if sseqid not in possible_seeds:
+			possible_seeds.append(sseqid)
+		if sseqid not in contigs_dict:
+			print(sseqid, "not in contigs_dict")
+		else:
+			if int(sstart) > int(send):
+				contigs_dict[sseqid]['Gene_coverage_intervals'].append((int(send), int(sstart)))
+			else:
+				contigs_dict[sseqid]['Gene_coverage_intervals'].append((int(sstart), int(send)))
+
+	for sseqid in contigs_dict:
+		union = get_union(contigs_dict[sseqid]['Gene_coverage_intervals'])
+		ln = contigs_dict[sseqid]['Length']
+		covered = 0
+		for interval in union:
+			covered += interval[1] - interval[0] + 1
+		contigs_dict[sseqid]['Gene_coverage'] = covered/ln
+
+	return contigs_dict	
+
+
+
+
+
+
+#-----------------------------------------------------------	
+#contigs[p][c] == 1 if contig 'c' belongs to plasmid 'p', else 0
+def contig_vars(m, contigs_dict, contigs, contigs_ext, nplasmids):
+	for p in range(nplasmids):
+		contigs[p] = {}
+		contigs_ext[p] = {}
+		for c in contigs_dict:
+			contigs[p][c] = m.addVar(vtype=GRB.BINARY, name='contig-'+c+'_plasmid-'+str(p))
+			contigs_ext[p][(c, 'h')] = m.addVar(vtype=GRB.BINARY, name='contigext-'+c+'h'+'_plasmid-'+str(p))
+			contigs_ext[p][(c, 't')] = m.addVar(vtype=GRB.BINARY, name='contigext-'+c+'t'+'_plasmid-'+str(p))
+	return contigs, contigs_ext
+
+#links[p][e] == 1 if link 'e' belongs to plasmid 'p', else 0
+def link_vars(m, links_list, links, nplasmids):
+	for p in range(nplasmids):
+		links[p] = {}
+		for e in links_list:		
+			links[p][e] = m.addVar(vtype=GRB.BINARY, name='link-'+e[0][0]+e[0][1]+e[1][0]+e[1][1]+'_plasmid-'+str(p))
+	return links
+
+#-------------------------------------------------------------
+def rd_vars(m, contigs_dict, rd, counted_rd, mean_rd, counted_mean_rd, nplasmids):
+	for p in range(nplasmids):
+		rd[p] = {}
+		counted_rd[p] = {}
+		counted_mean_rd[p] = {}
+		mean_rd[p] = m.addVar(vtype=GRB.CONTINUOUS, name='mean-rd-plasmid-'+str(p))
+		for c in contigs_dict:	
+			rd[p][c] = m.addVar(vtype=GRB.CONTINUOUS, name='rd_contig-'+c+'_plasmid-'+str(p))
+			counted_rd[p][c] = m.addVar(vtype=GRB.CONTINUOUS, name='counted-rd_contig-'+c+'_plasmid-'+str(p))
+			counted_mean_rd[p][c] = m.addVar(vtype=GRB.CONTINUOUS, name='counted-mean-rd_contig-'+c+'_plasmid-'+str(p))
+	return rd, counted_rd, mean_rd, counted_mean_rd
+
+def ln_vars(m, contigs_dict, counted_ln, nplasmids):
+	for p in range(nplasmids):
+		counted_ln[p] = {}	
+		for c in contigs_dict:
+			counted_ln[p][c] = m.addVar(vtype=GRB.INTEGER, name='counted-ln_contig-'+c+'_plasmid-'+str(p))
+	return counted_ln		
+
+def seed_vars(m, contigs_dict, counted_seed, nplasmids):
+	for p in range(nplasmids):
+		counted_seed[p] = {}
+		for c in contigs_dict:		
+			counted_seed[p][c] = m.addVar(vtype=GRB.INTEGER, name='counted-seed_contig-'+c+'_plasmid-'+str(p))
+	return counted_seed
+
+def diff_vars(m, contigs_dict, diff, counted_diff, nplasmids):
+	for p in range(nplasmids):
+		diff[p] = {}
+		counted_diff[p] = {}
+		for c in contigs_dict:
+			diff[p][c] = m.addVar(vtype=GRB.INTEGER, name='diff_contig-'+c+'_plasmid-'+str(p))
+			counted_diff[p][c] = m.addVar(vtype=GRB.INTEGER, name='counted-diff_contig-'+c+'_plasmid-'+str(p))
+	return diff, counted_diff	
+
+#-----------------------------------------------------------------------		
+
+
+
+
+
+
+			
+
+
---
+++ exp/2019-03-07__iterative_MILP/src/eval_scripts/analyse_sample.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+# MILP strategy
+#
+
+import os
+import subprocess
+
+
+proj_dir = '/home/amane/projects/rrg-chauvec/wg-anoph/Plasmids-Assembly'
+compare_script = proj_dir + '/exp/2019-03-07__iterative_MILP/src/eval_scripts/compare_plasmids.py'
+extract_script = proj_dir + '/data/2018-05-17__MOB-suite_benchmark/extract.sh'
+sample_script = proj_dir + '/data/2018-05-23__MOB-suite_benchmark_reads/sample.sh'
+
+#load_modules = 'module load gcc/5.4.0 blast+/2.6.0 boost/1.60.0 sickle/1.33 samtools/1.5 bowtie2/2.3.3.1 racon/20170719 perl/5.22.4 java/1.8.0_121 python/3.5.4; source $HOME/py3.5.4/bin/activate;'
+#unload_modules = 'source deactivate; module unload python/3.5.4 java/1.8.0_121 perl/5.22.4 boost/1.60.0 blast+/2.6.0 gcc/5.4.0;'
+    
+def evaluate(sample_id, out_dir, results):
+    p = subprocess.Popen('bash %s chr %s' % (sample_script, sample_id), stdout = subprocess.PIPE, shell = True)
+    output, _ = p.communicate()
+    p.wait()
+    chromosomes = output.rstrip().decode().split(',')    
+
+    p = subprocess.Popen('bash %s pla %s' % (sample_script, sample_id), stdout = subprocess.PIPE, shell = True)
+    output, _ = p.communicate()
+    p.wait()
+    plasmids = output.rstrip().decode().split(',')    
+    
+    eval_dir = out_dir + '/eval'
+    subprocess.call('mkdir %s' % eval_dir, shell = True)
+    chr_references_fasta = eval_dir + '/chromosomes.fasta'
+    pla_references_fasta = eval_dir + '/plasmids.fasta'
+    subprocess.call('rm -f %s %s' % (chr_references_fasta, pla_references_fasta), shell = True)
+    for acc in chromosomes:
+        file = '%s/%s.fasta' % (out_dir, acc)
+        subprocess.call('bash %s %s %s; cat %s >> %s; rm %s' % (extract_script, acc, file, file, chr_references_fasta, file), shell = True)
+    for acc in plasmids:
+        file = '%s/%s.fasta' % (out_dir, acc)
+        subprocess.call('bash %s %s %s; cat %s >> %s; rm %s' % (extract_script, acc, file, file, pla_references_fasta, file), shell = True)
+
+    blast_db = eval_dir + '/plasmid_references'
+    subprocess.call('makeblastdb -in %s -dbtype nucl -out %s; ' % (pla_references_fasta, blast_db), shell = True)
+    
+    quast_labels = []
+    quast_files = []
+    
+    
+    ## evaluate MILP-based plasmids
+    # first, map plasmid genes to Unicycler assembly and filter the mapping
+    print('Performing evaluations with MILP strategy...')
+    subprocess.call('mkdir %s/MILP' % eval_dir, shell = True)
+    
+    # MILP / partial training                     
+    MILP_putative_queries = out_dir + '/MILP/putative_plasmids.fasta'
+    MILP_putative_mapping = eval_dir + '/MILP/MILP_putative_map.csv'
+    #greedy_questionable_queries = out_dir + '/greedy/plasmids/greedy/questionable_plasmids.fasta'
+    #greedy_questionable_mapping = eval_dir + '/greedy/greedy_questionable_map.csv'
+    subprocess.call( 'blastn -task megablast -query %s -db %s -out %s -outfmt 6; ' % (MILP_putative_queries, blast_db, MILP_putative_mapping) \
+                    #+ 'blastn -task megablast -query %s -db %s -out %s -outfmt 6; ' % (greedy_questionable_queries, blast_db, greedy_questionable_mapping) \
+                    + 'python %s %s %s %s %s/MILP/MILP_eval.csv -i "MILP_putative;%s;%s" >> %s; ' % (compare_script, sample_id, chr_references_fasta, pla_references_fasta, eval_dir, MILP_putative_queries, MILP_putative_mapping, results) , shell = True)
+
+if __name__ == '__main__':
+    import argparse
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('sample_id', help = 'sample id of MOB-suite sample')
+    argparser.add_argument('out_dir', help = 'output directory')
+    argparser.add_argument('results', help = 'CSV file of scores')
+    args = argparser.parse_args()
+    
+    evaluate(args.sample_id, args.out_dir, args.results)
+
+    print('Finished analysis of sample %s.' % args.sample_id)
